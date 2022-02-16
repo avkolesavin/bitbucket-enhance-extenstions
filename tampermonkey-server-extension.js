d568a9ad80edddfd975baf0014976a0a4de53f2b
Aleksandr_Kolesavin <aleksandr_kolesavin@epam.com>
2022-02-16 17:10:50 +0300

    config: update server extension metadata

--- tampermonkey-server-extension.js
+++ tampermonkey-server-extension.js
@@ -3,6 +3,9 @@
 // @description  Connects to the local server, loads a provided extension script and eval it on the page. This allows to see fresh changes immediately.
 // @namespace    http://tampermonkey.net/
 // @author       Aleksandr Kolesavin
+// @version      1.0
+// @updateURL    https://raw.githubusercontent.com/avkolesavin/bitbucket-enhance-extenstions/main/tampermonkey-server-extension.js
+// @downloadURL  https://raw.githubusercontent.com/avkolesavin/bitbucket-enhance-extenstions/main/tampermonkey-server-extension.js
 // @grant        none
 // ==/UserScript==
 
