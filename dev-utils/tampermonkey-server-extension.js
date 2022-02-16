280b80be0872b190525794501da12fba122c2dac
Aleksandr_Kolesavin <aleksandr_kolesavin@epam.com>
2022-02-16 17:53:28 +0300

    docs: update server extension version

--- dev-utils/tampermonkey-server-extension.js
+++ dev-utils/tampermonkey-server-extension.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Developing server connector
-// @version      1.0
+// @version      1.0.1
 // @description  Connects to the local server, loads a provided extension script and eval it on the page. This allows to see fresh changes immediately.
 // @author       Aleksandr Kolesavin
 // @updateURL    https://raw.githubusercontent.com/avkolesavin/bitbucket-enhance-extenstions/main/dev-utils/tampermonkey-server-extension.js
